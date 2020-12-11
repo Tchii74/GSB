ff1ba74d304cd7f85ea84ad9810c6337099500f9
Tchii74 <75476192+Tchii74@users.noreply.github.com>
2020-12-11 15:57:10 +0100

    Create c_validerFicheFrais.php
    debut création constructeur ValiderFicheFrais

---
+++ controleurs/c_validerFicheFrais.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Gestion de la validation d'une fiche de frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Audrey Laval <audreylaval074@gmail.com>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ */
+
+// choisir le visiteur et le mois concern�
+
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+switch ($action) {
+    case 'afficherLesVisiteurs':
+        $lesVisiteurs = $spo->getTousLesVisiteurs
+    case 'afficherLesMois':
+        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+        // Afin de sélectionner par défaut le dernier mois dans la zone de liste
+        // on demande toutes les clés, et on prend la première,
+        // les mois étant triés décroissants
+        $lesCles = array_keys($lesMois);
+        $moisASelectionner = $lesCles[0];
+        include 'vues/v_listeMois.php';
+        break;
+    case 'voirEtatFrais':
+        $leMois = filter_input(INPUT_POST, 'lstMois', FILTER_SANITIZE_STRING);
+        $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+        $moisASelectionner = $leMois;
+        include 'vues/v_listeMois.php';
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
+        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
+        $numAnnee = substr($leMois, 0, 4);
+        $numMois = substr($leMois, 4, 2);
+        $libEtat = $lesInfosFicheFrais['libEtat'];
+        $montantValide = $lesInfosFicheFrais['montantValide'];
+        $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+        $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
+        include 'vues/v_etatFrais.php';
+}
